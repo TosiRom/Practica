0d32ec141db8e52bc2ad29ed22639085a674f44d
Sotir Cosmin <sotircosmin22@gmail.com>
2024-07-15 14:24:47 +0300

    angajati Table

---
+++ Practica/src/main/java/com/tosirom/practica/models/AngajatiTableModel.java
@@ -0,0 +1,70 @@
+package com.tosirom.practica.models;
+import com.tosirom.practica.database.Angajati;
+import java.util.ArrayList;
+import javax.swing.table.AbstractTableModel;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class AngajatiTableModel  extends AbstractTableModel {
+    protected static String[] COLUMN_NAMES = {"ID", "Nume", "Prenume","Functie","Data Angajare"};
+    protected static Class[] COLUMN_CLASSES = {Integer.class, String.class, String.class, String.class, String.class,};        
+    
+    private Set<Integer> selected;
+    private List<Angajati> inventar;
+    
+    public AngajatiTableModel() {
+        selected = new TreeSet<>();
+        inventar = new ArrayList<>(25);
+    }
+    
+    @Override
+    public int getRowCount() {
+        return inventar.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return COLUMN_NAMES.length;
+    }
+    
+    @Override
+    public String getColumnName(int column) {
+        return COLUMN_NAMES[column];
+    }
+    
+    @Override
+    public Class<?> getColumnClass(int columnIndex) {
+        return COLUMN_CLASSES[columnIndex];
+    }
+    
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return false;
+    }
+
+     @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        Angajati client = inventar.get(rowIndex);
+        switch (columnIndex) {
+            case 0: return client.ID;
+            case 1: return client.Nume;
+            case 2: return client.Prenume;
+            case 3: return client.Functie;
+            case 4: return client.Data_Angajare;
+            
+        }
+        return null;
+    }
+    
+     public void add(Angajati client) {
+        int index = inventar.size();
+        inventar.add(client);
+        fireTableRowsInserted(index, index);
+    }
+
+    public Angajati cardAt(int rowIndex) {
+        return inventar.get(rowIndex);
+    }
+    
+}
